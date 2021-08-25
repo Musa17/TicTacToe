3144a983c3d76c33ad7cb757a94e6ec201ea068d
Musa17 <amusul00@fesb.hr>
2021-08-25 14:57:34 +0200

    Making a list and setting default values for buttons

--- Tic Tac Toe/MainWindow.xaml.cs
+++ Tic Tac Toe/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Tic_Tac_Toe
 {
@@ -20,9 +11,52 @@ namespace Tic_Tac_Toe
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Private Members
+
+        private Marktype[] mResults;
+        private bool mPlayer1Turn;
+        private bool mGameEnded;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+
+            NewGame();
+        }
+
+        #endregion
+
+        private void NewGame()
+        {
+            mResults = new Marktype[9];
+
+            for (var i = 0; i < mResults.Length; i++)
+            {
+                mResults[i] = Marktype.Free;
+            }
+
+            mPlayer1Turn = true;
+
+            Container.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+                button.Content = string.Empty;
+                button.Background = Brushes.White;
+                button.Foreground = Brushes.Blue;
+            });
+
+            mGameEnded = false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Tic Tac Toe/MarkType.cs
@@ -0,0 +1,9 @@
+﻿namespace Tic_Tac_Toe
+{
+    public enum Marktype
+    {
+        Free,
+        Nought,
+        Cross
+    }
+}
